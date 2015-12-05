a7c6b26a1db784b9d52f8d60fbf873d84702e4ea
Seung Ho Jang <shjang1992@gmail.com>
2015-12-06 03:04:41 +0900

    Added all-in-one header

---
+++ include/LibMighty.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// All-in-one header file
+#include "BuildMacro.h"
+
+#include "Card.h"
+#include "CardRank.h"
+#include "CardRole.h"
+#include "CardSuit.h"
+#include "CardType.h"
+#include "CardUtilFunc.h"
+#include "Game.h"
+#include "HumanPlayer.h"
+#include "JokerCallRole.h"
+#include "JokerRole.h"
+#include "MightyRole.h"
+#include "NormalRole.h"
+#include "Round.h"
+#include "Rule.h"
