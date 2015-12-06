c77ac492578083e43ff868eda94812ad5088f7ad
Seung Ho Jang <shjang1992@gmail.com>
2015-12-06 16:07:59 +0900

    File rename

---
+++ include/libMighty.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// All-in-one header file
+#include "libMighty_BuildMacro.h"
+
+#include "libMighty_Card.h"
+#include "libMighty_CardRank.h"
+#include "libMighty_CardRole.h"
+#include "libMighty_CardSuit.h"
+#include "libMighty_CardType.h"
+#include "libMighty_CardUtilFunc.h"
+#include "libMighty_Game.h"
+#include "libMighty_HumanPlayer.h"
+#include "libMighty_JokerCallRole.h"
+#include "libMighty_JokerRole.h"
+#include "libMighty_MightyRole.h"
+#include "libMighty_NormalRole.h"
+#include "libMighty_Round.h"
+#include "libMighty_Rule.h"
