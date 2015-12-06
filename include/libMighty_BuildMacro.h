35b2e8d8a3f6e25b15b90c84000b8490dd5a7029
Seung Ho Jang <shjang1992@gmail.com>
2015-12-06 22:54:15 +0900

    Added dll build macro config

--- include/libMighty_BuildMacro.h
+++ include/libMighty_BuildMacro.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #ifdef _WIN32
-	#ifdef libMighty_EXPORTS
-		#define LIBMIGHTY_API __declspec(dllexport)
+	#ifdef libMighty_USE_DLL
+		#ifdef libMighty_EXPORTS
+			#define LIBMIGHTY_API __declspec(dllexport)
+		#else
+			#define LIBMIGHTY_API __declspec(dllimport)
+		#endif
 	#else
-		#define LIBMIGHTY_API __declspec(dllimport)
-	#endif
+		#define LIBMIGHTY_API
+#endif
 #else
 	#define LIBMIGHTY_API
 #endif
